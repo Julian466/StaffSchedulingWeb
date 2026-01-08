f02a525c95a99aad6e8ae02e16053a112ded8ead
Julian Bodden <julian11.bodden@gmail.com>
2026-01-08 15:23:04 +0100

    feat: update workflow starter script to streamline execution and open application in browser

--- start-workflow-with-env.bat
+++ start-workflow-with-env.bat
@@ -1,21 +1,6 @@
 @echo off
-REM Workflow-Starter für TimeOffice Integration
-REM Parameter: %1 = Startdatum (DD.MM.YYYY)
-REM            %2 = Enddatum (DD.MM.YYYY)
-REM            %3 = Case-Nummer
+REM Execute npm run dev -- --WORKFLOW_MODE=true --WORKFLOW_CASE=120 --WORKFLOW_START=01.02.2024 --WORKFLOW_END=30.02.2024
+npm run dev -- --WORKFLOW_MODE=true --WORKFLOW_CASE=%3 --WORKFLOW_START=%1 --WORKFLOW_END=%2
 
-REM Pfad zur Anwendung (verwendet das Verzeichnis, in dem das Script liegt)
-set APP_PATH=%~dp0
-
-echo ========================================
-echo Dienstplan-Workflow wird gestartet
-echo ========================================
-echo Planungseinheit: %3
-echo Von: %1
-echo Bis: %2
-echo ========================================
-echo.
-
-REM Starte PowerShell-Script für besseres Cleanup
-cd /d %APP_PATH%
-powershell -ExecutionPolicy Bypass -File "start-workflow.ps1" -StartDate "%1" -EndDate "%2" -CaseId "%3"
+REM Open the browser to view the application
+start http://localhost:3000/workflow
