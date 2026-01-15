0b72ce36ee1c2aa95d2cb851a60ab7a057f1b7f6
Julian Bodden <julian11.bodden@gmail.com>
2026-01-15 22:15:13 +0100

    feat: add custom start script with argument parsing for Next.js server

--- start-workflow-with-env.bat
+++ start-workflow-with-env.bat
@@ -1,6 +1,7 @@
 @echo off
-REM Execute npm run dev -- --WORKFLOW_MODE=true --WORKFLOW_CASE=120 --WORKFLOW_START=01.02.2024 --WORKFLOW_END=30.02.2024
-npm run dev -- --WORKFLOW_MODE=true --WORKFLOW_CASE=%3 --WORKFLOW_START=%1 --WORKFLOW_END=%2
-
 REM Open the browser to view the application
 start http://localhost:3000/workflow
+
+
+REM Execute npm run start -- --WORKFLOW_MODE=true --WORKFLOW_CASE=120 --WORKFLOW_START=01.02.2024 --WORKFLOW_END=30.02.2024
+npm run start -- --WORKFLOW_MODE=true --WORKFLOW_CASE=%3 --WORKFLOW_START=%1 --WORKFLOW_END=%2
