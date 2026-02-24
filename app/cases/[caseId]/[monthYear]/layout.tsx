1dc06ad7097d6c4b07befad91654c98a552e6998
Julian466 <julian.bodden@rwth-aachen.de>
2026-02-24 18:24:10 +0100

    Improve validation error message in layout

--- app/cases/[caseId]/[monthYear]/layout.tsx
+++ app/cases/[caseId]/[monthYear]/layout.tsx
@@ -13,7 +13,7 @@ export default async function CaseLayout({ children, params }: CaseLayoutProps) 
   }
   
   if (!/^\d{1,2}_\d{4}$/.test(monthYear)) {
-    return <div>Invalid month/year format</div>;
+    return <div>Invalid month/year format. Expected format: M_YYYY or MM_YYYY</div>;
   }
   
   return <>{children}</>;
