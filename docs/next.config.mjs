b12f68f94d647555da88504dfee6329dadc0475b
Julian Bodden <julian11.bodden@gmail.com>
2026-03-08 19:36:17 +0100

    feat: configure PostCSS and enhance Next.js settings for StaffSchedulingWeb

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,14 +1,22 @@
 import nextra from 'nextra'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 /**
  * @type {import('next').NextConfig}
  */
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 const nextConfig = {
     output: 'export',
     basePath: '/StaffSchedulingWeb',
     images: {
         unoptimized: true // mandatory, otherwise won't export
-    }
+    },
+    turbopack: {
+        root: __dirname,  // ← verhindert, dass Turbopack das Repo-Root nimmt
+    },
     // Optional: Change the output directory `out` -> `dist`
     // distDir: "build"
 }
