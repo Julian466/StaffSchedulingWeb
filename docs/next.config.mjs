d2e0d80cdd072bcee721242b14bb8a17b76ecc8a
Julian Bodden <julian11.bodden@gmail.com>
2026-03-08 20:04:37 +0100

    feat: configure Next.js settings for GitHub Actions and local development

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,25 +2,37 @@ import nextra from 'nextra'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 /**
  * @type {import('next').NextConfig}
  */
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
 
-const nextConfig = {
+const nextConfigLocal = {
     output: 'export',
+    images: {
+        unoptimized: true // mandatory, otherwise won't export
+    },
+}
+
+const nextConfigGithubActions = {
     basePath: '/StaffSchedulingWeb',
+    output: 'export',
     images: {
         unoptimized: true // mandatory, otherwise won't export
     },
     turbopack: {
-        root: __dirname,  // ← verhindert, dass Turbopack das Repo-Root nimmt
+        // Optional: Disable Turbopack for development
+        // dev: false,
+        root: __dirname,
     },
     // Optional: Change the output directory `out` -> `dist`
     // distDir: "build"
 }
+
 const withNextra = nextra({
+    // ... other Nextra config options
 })
 
-export default withNextra(nextConfig)
+export default withNextra(isGithubActions ? nextConfigGithubActions : nextConfigLocal)
